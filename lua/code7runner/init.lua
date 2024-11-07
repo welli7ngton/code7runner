7c8727f9f5c953b568e8620f9ba132d1234c02c8
Wellington - Trabalho <wellingtonasilva45@gmail.com>
2024-11-07 13:10:56 -0300

    refactor: solve logic problems and create a better intergated terminal, fix some path problems

--- lua/code7runner/init.lua
+++ lua/code7runner/init.lua
@@ -1,33 +1,39 @@
 local user_cmds = require('code7runner.user_cmds')
 
 user_cmds.setup()
+
 local M = {}
 
 function M.run()
-  local filename = vim.fn.expand("%:t")
-  local dir = vim.fn.expand("%:p:h")
-  local filename_without_ext = vim.fn.expand("%:t:r")
-  local extension = filename:lower():match('^.+%.(.+)$')
+  local file_path = vim.fn.expand('%:p')
+  local project_dir = vim.fn.fnamemodify(file_path, ':h')
+  local filename = vim.fn.fnamemodify(file_path, ':t')
+  local filename_without_ext = vim.fn.fnamemodify(filename, ':r')
+  local extension = vim.fn.fnamemodify(filename, ':e')
 
   local executor_map = {
-    js = 'clear && node ' .. filename,
-    ts = 'clear && ts-node ' .. filename,
-    py = 'clear && python -u ' .. filename,
-    java = 'cd ' .. dir .. ' && javac ' .. filename .. ' && java ' .. filename_without_ext,
+    js = 'clear && node ' .. file_path,
+    ts = 'clear && ts-node ' .. file_path,
+    py = 'clear && python3 ' .. file_path,
+    java = 'cd ' .. project_dir .. ' && javac ' .. filename .. ' && java ' .. filename_without_ext,
     c = 'cd ' ..
-        dir .. ' && gcc ' .. filename .. ' -o ' .. filename_without_ext .. ' && ' .. dir .. '/' .. filename_without_ext,
+        project_dir ..
+        ' && gcc ' .. filename .. ' -o ' .. filename_without_ext .. ' && ' .. project_dir .. '/' .. filename_without_ext,
     cpp = 'cd ' ..
-        dir .. ' && g++ ' .. filename .. ' -o ' .. filename_without_ext .. ' && ' .. dir .. '/' .. filename_without_ext,
-    robot = 'clear && robot ' .. filename,
-    lua = 'clear && lua ' .. filename,
-    php = 'clear && php ' .. filename,
-    rb = 'clear && ruby ' .. filename,
+        project_dir ..
+        ' && g++ ' .. filename .. ' -o ' .. filename_without_ext .. ' && ' .. project_dir .. '/' .. filename_without_ext,
+    robot = 'clear && robot ' .. '-d ' .. project_dir .. '/results ' .. file_path,
+    lua = 'clear && lua ' .. file_path,
+    php = 'clear && php ' .. file_path,
+    rb = 'clear && ruby ' .. file_path,
   }
 
   local action = executor_map[extension]
   if action then
-    -- Configuração da janela flutuante
+    vim.notify(action, vim.log.levels.INFO)
+
     local buf = vim.api.nvim_create_buf(false, true)
+
     local width = math.floor(vim.o.columns * 0.8)
     local height = math.floor(vim.o.lines * 0.8)
     local opts = {
@@ -40,7 +46,8 @@ function M.run()
       border = 'rounded',
     }
 
-    vim.api.nvim_open_win(buf, true, opts)
+    local win = vim.api.nvim_open_win(buf, true, opts)
+
     vim.fn.termopen(action)
   else
     print("Nenhum executor configurado para a extensão: " .. extension)
@@ -48,4 +55,3 @@ function M.run()
 end
 
 return M
-
